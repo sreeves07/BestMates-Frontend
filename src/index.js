bc2fa37886de4336c41a1081a18cdff9ca448e54
bowersjuan <bowersjuan@gmail.com>
2023-05-12 09:16:37 -0400

    Update index.js with AuthProvider and Router components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import AuthProvider from './Firebase/context';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <React.StrictMode>
+      <AuthProvider>
+         <App />
+      </AuthProvider>
+    </React.StrictMode>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
