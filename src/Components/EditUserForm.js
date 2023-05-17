aeedfe36dcd319a2a40e10145bc4268ce4ec2169
D-Stukes <dstukes888@gmail.com>
2023-05-17 04:38:07 -0400

    updated classnames for EditFormUser

--- src/Components/EditUserForm.js
+++ src/Components/EditUserForm.js
@@ -63,8 +63,7 @@ const updateUser = (updatedUser) => {
 
   return (
     <div className='editFormBox'>
-
-        {/* ****** Grid Row 2  ***** */}
+ 
         {/* Form headings */}
          <div className="editFormHdgBox">
             <span className='editFormHdg'>Edit Profile Form</span> <br/>
@@ -76,14 +75,14 @@ const updateUser = (updatedUser) => {
            {/* ****** Grid Row 1  ***** */}
            {/* Basic Info Heading */}
 
-         <h6 className="basicInfoHdg">Basic Information:</h6>
+         <h6 className="basicInfoHdg-ef">Basic Information:</h6>
 
 
            {/* ****** Grid Row 2  ***** */}
            {/* Basic Info */}
-          <label className='editFormLabel basicInfo1-firstName'   htmlFor="firstName">First Name: </label>
+          <label className='editFormLabel basicInfo1-firstName-ef'   htmlFor="firstName">First Name: </label>
           <input 
-                className='firstName'
+                className='firstName-ef'
                 id="first_name" 
                 name="firstName" 
                 type="text" 
@@ -91,9 +90,9 @@ const updateUser = (updatedUser) => {
                 value={user.first_name}
                 required
                 />
-          <label className='editFormLabel basicInfo2-lastName' htmlFor="lastName">Last Name: </label>
+          <label className='editFormLabel basicInfo2-lastName-ef' htmlFor="lastName">Last Name: </label>
           <input 
-                className='lastName'
+                className='lastName-ef'
                 id="last_name" 
                 name="lastName" 
                 type="text" 
@@ -101,9 +100,9 @@ const updateUser = (updatedUser) => {
                 value={user.last_name}
                 />
 
-          <label className='editFormLabel basicInfo3-birthDate'  htmlFor="birthDate">Date of Birth: </label>
+          <label className='editFormLabel basicInfo3-birthDate-ef'  htmlFor="birthDate">Date of Birth: </label>
           <input 
-                className='birthDate'
+                className='birthDate-ef'
                 id="birthday" 
                 name="birthDate" 
                 type="date" 
@@ -114,9 +113,9 @@ const updateUser = (updatedUser) => {
            {/* ****** Grid Row 2 ***** */}
            {/* Basic Info Continued */}
            
-          <label className='editFormLabel basicInfo4-city'  htmlFor="city">City: </label>
+          <label className='editFormLabel basicInfo4-city-ef'  htmlFor="city">City: </label>
           <input 
-                className='city'
+                className='city-ef'
                 id="city" 
                 name="city" 
                 type= "text"
@@ -124,9 +123,9 @@ const updateUser = (updatedUser) => {
                 value={user.city}
                 />
 
-          <label className='editFormLabel basicInfo5-state'  htmlFor="state">State: </label>
+          <label className='editFormLabel basicInfo5-state-ef'  htmlFor="state">State: </label>
           <input 
-                className='state'
+                className='state-ef'
                 id="state" 
                 name="state" 
                 type="text" 
@@ -134,27 +133,27 @@ const updateUser = (updatedUser) => {
                 value={user.state}
                 />
 
-         <label className='editFormLabel basicInfo6-zipcode' htmlFor="zip_code">Zip Code: </label>
+         <label className='editFormLabel basicInfo6-zipcode-ef' htmlFor="zip_code">Zip Code: </label>
           <input 
-                className='zipcode'
+                className='zipcode-ef'
                 id="zip_code" 
                 name="zip_code" 
                 type="number"
                 onChange={handleTextChange}
                 value={user.zip_code}
                 />
-          <label className='editFormLabel basicInfo7-email' htmlFor="email">Email: </label>
+          <label className='editFormLabel basicInfo7-email-ef' htmlFor="email">Email: </label>
           <input 
-                className='email'
+                className='email-ef'
                 id="email" 
                 name="email" 
                 type="email" 
                 onChange={handleTextChange}
                 value={user.email}
                 />        
-          <label className='editFormLabel basicInfo8-gender'  htmlFor="gender">Gender: </label>
+          <label className='editFormLabel basicInfo8-gender-ef'  htmlFor="gender">Gender: </label>
           <input 
-                className='gender'
+                className='gender-ef'
                 id="gender" 
                 name="gender" 
                 type="text" 
@@ -165,36 +164,36 @@ const updateUser = (updatedUser) => {
           {/* ****** Grid Row 4 ***** */}
           {/* Attributes - text fields and dates */}
           
-          <label className='editFormLabel attribute1-income' htmlFor="income">Income: </label>
+          <label className='editFormLabel attribute1-income-ef' htmlFor="income">Income: </label>
           <input 
-                className='income'
+                className='income-ef'
                 id="income" 
                 name="income" 
                 type="number" 
                 onChange={handleTextChange}
                 value={user.income}
                 />
-          <label className='editFormLabel attribute2-creditScore'  htmlFor="creditScore">Credit Score: </label>
+          <label className='editFormLabel attribute2-creditScore-ef'  htmlFor="creditScore">Credit Score: </label>
           <input 
-                className='creditScore'
+                className='creditScore-ef'
                 id="credit_score" 
                 name="creditScore" 
                 type="number" 
                 onChange={handleTextChange}
                 value={user.credit_score}
                 />
-          <label className='editFormLabel attribute3-maxRent'  htmlFor="maxRent">Maximum Rent: </label>
+          <label className='editFormLabel attribute3-maxRent-ef'  htmlFor="maxRent">Maximum Rent: </label>
           <input 
-                className='maxRent'
+                className='maxRent-ef'
                 id="max_rent" 
                 name="maxRent" 
                 type="number" 
                 onChange={handleTextChange}
                 value={user.max_rent}
                 />
-         <label className='editFormLabel basicInfo9-sexOrient'  htmlFor="sexOrient">Sexual Orientation: </label>
+         <label className='editFormLabel basicInfo9-sexOrient-ef'  htmlFor="sexOrient">Sexual Orientation: </label>
           <input 
-                className='sexOrient'
+                className='sexOrient-ef'
                 id="sexual_orientation" 
                 name="sexOrient" 
                 type="text" 
@@ -205,16 +204,16 @@ const updateUser = (updatedUser) => {
          {/* ****** Grid Row 5 ***** */}
          {/* Attribute headings*/}
 
-         <h6 className="atttributeHdg">Your Attributes:</h6> 
-         <h6 className="atttributeSubHdg">(This information will allow other roomates to find you as a match.)</h6>
+         <h6 className="atttributeHdg-ef">Your Attributes:</h6> 
+         <h6 className="atttributeSubHdg-ef">(This information will allow other roomates to find you as a match.)</h6>
          
 
          {/* ****** Grid Row 6 ***** */}
           {/* Attributes - Checkboxes - Boolean Values */}
 
-          <label className='editFormLabel attribute4-moveInDate'  htmlFor="moveInDate">Move-in<br/> Date: </label>
+          <label className='editFormLabel attribute4-moveInDate-ef'  htmlFor="moveInDate">Move-in<br/> Date: </label>
           <input 
-                className='moveInDate'
+                className='moveInDate-ef'
                 id="move_in_date" 
                 name="moveInDate" 
                 type="date" 
@@ -222,9 +221,9 @@ const updateUser = (updatedUser) => {
                 value={user.move_in_date}
                 />
       
-          <label  className='editFormLabel attribute5-isSharingBills' htmlFor="isSharingBills"> Shares Utility Bills</label>
+          <label  className='editFormLabel attribute5-isSharingBills-ef' htmlFor="isSharingBills"> Shares Utility Bills</label>
           <input 
-                className='isSharingBills'
+                className='isSharingBills-ef'
                 id="is_sharing_bills" 
                 name="isSharingBills" 
                 type="checkbox" 
@@ -232,9 +231,9 @@ const updateUser = (updatedUser) => {
                 value={user.is_sharing_bills}
                 />
 
-          <label className='editFormLabel attribute6-hasOpenRooms'  htmlFor="hasOpenRooms">Has Open Rooms: </label>
+          <label className='editFormLabel attribute6-hasOpenRooms-ef'  htmlFor="hasOpenRooms">Has Open Rooms: </label>
           <input 
-                className='hasOpenRooms'
+                className='hasOpenRooms-ef'
                 id="has_open_rooms" 
                 name="hasOpenRooms" 
                 type="checkbox" 
@@ -242,9 +241,9 @@ const updateUser = (updatedUser) => {
                 value={user.has_open_rooms}
                 /><br/><br/>
 
-          <label className='editFormLabel attribute10-isDisabled'  htmlFor="disabled">Has Disability: </label>
+          <label className='editFormLabel attribute10-isDisabled-ef'  htmlFor="disabled">Has Disability: </label>
           <input 
-                className='isDisabled'
+                className='isDisabled-ef'
                 id="is_disabled" 
                 name="disabled" 
                 type="checkbox" 
@@ -254,18 +253,18 @@ const updateUser = (updatedUser) => {
 
           {/* ****** Grid Row 7 ***** */}
 
-         <label className='editFormLabel attribute7-hasKids'  htmlFor="hasKids">Has Kids: </label>
+         <label className='editFormLabel attribute7-hasKids-ef'  htmlFor="hasKids">Has Kids: </label>
           <input 
-                className='hasKids'
+                className='hasKids-ef'
                 id="has_kids" 
                 name="hasKids" 
                 type="checkbox" 
                 onChange={ (event) => handleCheckboxChange(event)}
                 value={user.has_kids}
                 />
-          <label className='editFormLabel attribute8-hasPets'  htmlFor="hasPets">Has Pets: </label>
+          <label className='editFormLabel attribute8-hasPets-ef'  htmlFor="hasPets">Has Pets: </label>
           <input 
-                className='hasPets'
+                className='hasPets-ef'
                 id="has_pets" 
                 name="hasPets" 
                 type="checkbox" 
@@ -273,9 +272,9 @@ const updateUser = (updatedUser) => {
                 value={user.has_pets}
                 />
 
-         <label  className='editFormLabel attribute11-isNeat' htmlFor="isNeat">Is Very neat? </label>
+         <label  className='editFormLabel attribute11-isNeat-ef' htmlFor="isNeat">Is Very neat? </label>
           <input 
-                className='isNeat'
+                className='isNeat-ef'
                 id="is_neat" 
                 name="isNeat" 
                 type="checkbox" 
@@ -283,9 +282,9 @@ const updateUser = (updatedUser) => {
                 value={user.is_neat}
                 />
 
-          <label className='editFormLabel attribute9-isSmoker'  htmlFor="isSmoker">Is A Smoker: </label>
+          <label className='editFormLabel attribute9-isSmoker-ef'  htmlFor="isSmoker">Is A Smoker: </label>
           <input 
-                className='isSmoker'
+                className='isSmoker-ef'
                 id="is_smoker" 
                 name="isSmoker" 
                 type="checkbox" 
@@ -294,9 +293,9 @@ const updateUser = (updatedUser) => {
                 /><br/><br/>
 
         
-          <label className='editFormLabel attribute12-isReligious'  htmlFor="isReligious">Is Religious: </label>
+          <label className='editFormLabel attribute12-isReligious-ef'  htmlFor="isReligious">Is Religious: </label>
           <input 
-                className='isReligious'
+                className='isReligious-ef'
                 id="is_religious:" 
                 name="isReligious" 
                 type="checkbox" 
--- src/Pages/Edit.js
+++ src/Pages/Edit.js
@@ -4,7 +4,7 @@ const Edit = () => {
   return (
     <div className="Edit">
       <h2 className="header">Edit your profile!</h2>
-      {/* <EditUserForm /> */}
+      <EditUserForm />
     </div>
   );
 };
