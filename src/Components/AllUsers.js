115f12ee225bf5d156ca2c9e01482455223a5ed8
sreeves07 <shamarreeves@pursuit.org>
2023-05-17 22:02:46 -0400

    post fixed and put fixed

--- src/Components/AllUsers.js
+++ src/Components/AllUsers.js
@@ -1,12 +1,14 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import User from "./User";
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import User from './User';
 
 const API = process.env.REACT_APP_API_URL;
 
 function AllUsers() {
   const [users, setUsers] = useState([]);
+  const [preferences, setPreferences] = useState([]);
+
   useEffect(() => {
     axios
       .get(`${API}/user`)
@@ -14,7 +16,18 @@ function AllUsers() {
         // console.log("user api response data=", response.data);
         setUsers(response.data);
       })
-      .catch((c) => console.warn("catch", c));
+      .catch((c) => console.warn('catch', c));
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(`${API}/user/:id/answers`)
+      .then((response) => {
+        console.log(response.data);
+        // console.log("user api response data=", response.data);
+        setPreferences(response.data);
+      })
+      .catch((c) => console.warn('catch', c));
   }, []);
 
   return (
