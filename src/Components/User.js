4e9675ba9d1c31e1c4304d26e366e1ff7009706c
bowersjuan <bowersjuan@gmail.com>
2023-06-05 19:57:27 -0400

    Updated User component styling hover effect for cards and default image

--- src/Components/User.js
+++ src/Components/User.js
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
+import { useContextAuthProvider } from '../Firebase/context';
 // import { useState, useEffect } from "react";
 // import axios from "axios";
 import "./User.css";
 
+import defaultProfilePic from "../Images/LOGO_favicon.png"
+
 // const API = process.env.REACT_APP_API_URL;
 
 const User = ({ currentUser }) => {
-  const { first_name, birthday, gender, id, profile_image } = currentUser;
+  const { user } = useContextAuthProvider()
+  const { first_name, birthday, gender, profile_image } = currentUser;
 
   const age = (birthday) => {
     const birthYearSplit = new Date(birthday).toString().split(" ");
@@ -18,21 +22,22 @@ const User = ({ currentUser }) => {
   return (
     <div>
       {/* provides link to single user card view */}
-      <Link to={`/users/${id}`}>
+      <Link to={`/users/${user.uid}`}>
         <div className="userCard">
           <img
             className="rounded-circle"
             style={{ width: "9.0rem", height: "9.0rem" }}
             alt="Avatar"
             id="profileImage"
-            src={profile_image}
+            src={profile_image || defaultProfilePic}
             // alt="user profile"
           />{" "}
           <br />
+          <br></br>
           <span className="userCard-fname">{first_name} </span> <br />
           <span className="userCard-info">
             {" "}
-            Age:{age(birthday)} {gender}
+            {gender} -  Age: {age(birthday)} 
           </span>{" "}
           <br />
         </div>
