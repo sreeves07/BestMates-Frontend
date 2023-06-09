afae7c1ba4babf60140b8530bcc72cfd2b92b5aa
D-Stukes <dstukes888@gmail.com>
2023-06-09 09:57:24 -0400

    updated alignment of items in Terms component

--- src/Pages/Terms.js
+++ src/Pages/Terms.js
@@ -1,56 +1,168 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+
+import {
+  MDBCol,
+  MDBRow,
+  MDBTypography,
+  MDBCard,
+  MDBCardBody,
+  MDBCardHeader,
+  MDBBtn,
+} 
+from "mdb-react-ui-kit";
 
 function Terms() {
   return (
+          <div className='terms-box .overflow-hidden'>
+            
+            <MDBCard className="terms-card col-10">
+              <MDBCardHeader className=" terms-headerCard py-2">
+                <MDBTypography tag="h4" className="mb-3 termsHdg" >Terms and Conditions of Use for BestMates</MDBTypography>
+              </MDBCardHeader>
+              
+                <MDBRow className="term-backBtn">
+                  <Link to={`/signin`}>
+                    <MDBBtn className='terms-backBtn'>Back</MDBBtn>
+                  </Link>
+                </MDBRow>
 
-    <div>
-        <h2>Terms and Conditions of Use for BestMates</h2>
-        <p>
+              <MDBCardBody className='terms-cardBody'>
+                <MDBRow className="mb-5 terms-intro">These Terms and Conditions ("Agreement") govern your use of BestMates ("App"), including any features, services, and content provided by the App.  By accessing or using the App, you agree to be bound by this Agreement. If you do not agree with these terms, please refrain from using the App.
+                </MDBRow>
 
-These Terms and Conditions ("Agreement") govern your use of BestMates ("App"), including any features, services, and content provided by the App. By accessing or using the App, you agree to be bound by this Agreement. If you do not agree with these terms, please refrain from using the App.
+                <MDBRow> <MDBTypography tag="h5" className="mb-4" >Account Registration</MDBTypography></MDBRow>
+                <MDBRow className="mb-3">
+                  <MDBCol md="1" className="mb-1">1.1 </MDBCol>
+                  <MDBCol md="9" >You must be at least 18 years old to use the App. By creating an account, you represent and warrant that you are of legal age.
+                  </MDBCol>   
+                </MDBRow>
+                <MDBRow className="mb-3">
+                  <MDBCol md="1" className="mb-1">1.2 </MDBCol>
+                  <MDBCol md="9">You are responsible for maintaining the confidentiality of your account credentials. You agree to notify us immediately of any unauthorized access or use of your account.
+                  </MDBCol>
+                </MDBRow>
+                <MDBRow className="mb-3">
+                  <MDBCol md="1" className="mb-1">1.3 </MDBCol>
+                  <MDBCol md="9" >You are responsible for any activity that occurs under your account. We are not liable for any loss or damage arising from your failure to comply with this section.
+                  </MDBCol>  <br/>
+                </MDBRow>
+                <MDBRow className="mb-3"> <MDBTypography tag="h5"  >User Conduct</MDBTypography></MDBRow> 
+                
+                <MDBRow className="mb-3">
+                  <MDBCol md="1" className="mb-1">2.1 </MDBCol>
+                  <MDBCol md="9" >You agree to use the App in compliance with all applicable laws and regulations.
+                  </MDBCol>   
+                </MDBRow>
+                <MDBRow className="mb-3">
+                  <MDBCol md="1" className="mb-1">2.2 </MDBCol>
+                  <MDBCol md="9" >You will not use the App for any unlawful, abusive, or harmful purpose, or in any way that violates this Agreement.
+                  </MDBCol>   
+                </MDBRow>
+                <MDBRow className="mb-3">
+                  <MDBCol md="1" className="mb-1">2.3 </MDBCol>
+                  <MDBCol md="9" >You will not engage in any fraudulent activity or impersonate any person or entity.
+                  </MDBCol>   
+                </MDBRow>
+                <MDBRow className="mb-3">
+                  <MDBCol md="1" className="mb-1">2.4 </MDBCol>
+                  <MDBCol md="9" >You will not transmit any viruses, malware, or other harmful code through the App.
+                  </MDBCol>   
+                </MDBRow>
 
-Account Registration
-1.1 You must be at least 18 years old to use the App. By creating an account, you represent and warrant that you are of legal age.
-1.2 You are responsible for maintaining the confidentiality of your account credentials. You agree to notify us immediately of any unauthorized access or use of your account.
-1.3 You are responsible for any activity that occurs under your account. We are not liable for any loss or damage arising from your failure to comply with this section.
+                <MDBRow> <MDBTypography tag="h5" className="mb-3" >User Content </MDBTypography></MDBRow>
+                <MDBRow className="mb-3">
+                  <MDBCol md="1" className="mb-1">3.1 </MDBCol>
+                  <MDBCol md="9" >By using the App, you may contribute content such as profiles, listings, messages, and reviews ("User Content").
+                  </MDBCol>   
+                </MDBRow>
+                <MDBRow className="mb-3">
+                  <MDBCol md="1" className="mb-1">3.2 </MDBCol>
+                  <MDBCol md="9" >You retain ownership of your User Content. However, by submitting User Content, you grant us a non-exclusive, worldwide, royalty-free license to use, reproduce, modify, distribute, and display your User Content for the purposes of operating and improving the App.
+                  </MDBCol>   
+                </MDBRow>
+                <MDBRow className="mb-3">
+                  <MDBCol md="1" className="mb-1">3.3 </MDBCol>
+                  <MDBCol md="9" >You represent and warrant that your User Content does not infringe any intellectual property rights or violate any third-party rights.
+                  </MDBCol>   
+                </MDBRow>
+                <MDBRow className="mb-3">
+                  <MDBCol md="1" className="mb-1">3.4 </MDBCol>
+                  <MDBCol md="9" >We reserve the right to remove or modify any User Content that violates this Agreement or is otherwise objectionable.
+                  </MDBCol>   
+                </MDBRow>
 
-User Conduct
-2.1 You agree to use the App in compliance with all applicable laws and regulations.
-2.2 You will not use the App for any unlawful, abusive, or harmful purpose, or in any way that violates this Agreement.
-2.3 You will not engage in any fraudulent activity or impersonate any person or entity.
-2.4 You will not transmit any viruses, malware, or other harmful code through the App.
+              <MDBRow> <MDBTypography tag="h5" className="mb-4 " >Privacy</MDBTypography></MDBRow>
+              <MDBRow className="mb-3">
+                  <MDBCol md="1" className="mb-1">4.1 </MDBCol>
+                  <MDBCol md="9" >We collect and process personal information in accordance with our Privacy Policy. By using the App, you consent to our collection and use of your personal information as described in the Privacy Policy.
+                  </MDBCol>   
+                </MDBRow>
 
-User Content
-3.1 By using the App, you may contribute content such as profiles, listings, messages, and reviews ("User Content").
-3.2 You retain ownership of your User Content. However, by submitting User Content, you grant us a non-exclusive, worldwide, royalty-free license to use, reproduce, modify, distribute, and display your User Content for the purposes of operating and improving the App.
-3.3 You represent and warrant that your User Content does not infringe any intellectual property rights or violate any third-party rights.
-3.4 We reserve the right to remove or modify any User Content that violates this Agreement or is otherwise objectionable.
+              <MDBRow> <MDBTypography tag="h5" className="mb-3" >Disclaimer of Warranties </MDBTypography></MDBRow>
+              <MDBRow className="mb-3">
+                  <MDBCol md="1" className="mb-1">5.1 </MDBCol>
+                  <MDBCol md="9" >The App is provided on an "as is" and "as available" basis. We make no warranties or representations, express or implied, regarding the App's availability, reliability, accuracy, or fitness for a particular purpose.
+                  </MDBCol>   
+                </MDBRow>
+                <MDBRow className="mb-2">
+                  <MDBCol md="1" className="mb-1">5.2 </MDBCol>
+                  <MDBCol md="9" >We do not endorse, guarantee, or verify the accuracy or authenticity of User Content or any other information provided by users of the App.
+                  </MDBCol>   
+                </MDBRow>
 
-Privacy
-4.1 We collect and process personal information in accordance with our Privacy Policy. By using the App, you consent to our collection and use of your personal information as described in the Privacy Policy.
+                <MDBRow> <MDBTypography tag="h5" className="mb-3" > Limitation of Liability </MDBTypography></MDBRow>
+                <MDBRow className="mb-3">
+                  <MDBCol md="1" className="mb-1">6.1 </MDBCol>
+                  <MDBCol md="9" >To the maximum extent permitted by law, we shall not be liable for any direct, indirect, incidental, consequential, or punitive damages arising out of or in connection with the use or inability to use the App.
+                  </MDBCol>   
+                </MDBRow>
+                <MDBRow className="mb-2">
+                  <MDBCol md="1" className="mb-1">6.2 </MDBCol>
+                  <MDBCol md="9" >We are not responsible for any disputes, interactions, or transactions between users of the App. You agree to release us from any claims, demands, or damages arising from such disputes.
+                  </MDBCol>   
+                </MDBRow>
 
-Disclaimer of Warranties
-5.1 The App is provided on an "as is" and "as available" basis. We make no warranties or representations, express or implied, regarding the App's availability, reliability, accuracy, or fitness for a particular purpose.
-5.2 We do not endorse, guarantee, or verify the accuracy or authenticity of User Content or any other information provided by users of the App.
+                <MDBRow> <MDBTypography tag="h5" className="mb-3" > Intellectual Property </MDBTypography></MDBRow>
+                <MDBRow className="mb-3">
+                  <MDBCol md="1" className="mb-1">7.1 </MDBCol>
+                  <MDBCol md="9" >The App and its content are protected by copyright, trademark, and other intellectual property laws. You may not modify, reproduce, distribute, or create derivative works based on the App or its content without our prior written consent.
+                  </MDBCol>   
+                </MDBRow>
 
-Limitation of Liability
-6.1 To the maximum extent permitted by law, we shall not be liable for any direct, indirect, incidental, consequential, or punitive damages arising out of or in connection with the use or inability to use the App.
-6.2 We are not responsible for any disputes, interactions, or transactions between users of the App. You agree to release us from any claims, demands, or damages arising from such disputes.
+                <MDBRow><MDBTypography tag="h5" className="mb-3">Modifications</MDBTypography></MDBRow>
+                <MDBRow className="mb-3">
+                  <MDBCol md="1" className="mb-1">8.1 </MDBCol>
+                  <MDBCol md="9">We reserve the right to modify or terminate the App or this Agreement at any time without prior notice. Any changes will be effective immediately upon posting.
+                  </MDBCol>   
+                </MDBRow>
+                <MDBRow className="mb-3">
+                  <MDBCol md="1" className="mb-1">8.2 </MDBCol>
+                  <MDBCol md="9" >It is your responsibility to review this Agreement periodically. Continued use of the App after any modifications constitutes acceptance of the updated terms.
+                  </MDBCol>   
+                </MDBRow>
 
-Intellectual Property
-7.1 The App and its content are protected by copyright, trademark, and other intellectual property laws. You may not modify, reproduce, distribute, or create derivative works based on the App or its content without our prior written consent.
-
-Modifications
-8.1 We reserve the right to modify or terminate the App or this Agreement at any time without prior notice. Any changes will be effective immediately upon posting.
-8.2 It is your responsibility to review this Agreement periodically. Continued use of the App after any modifications constitutes acceptance of the updated terms.
-
-Governing Law
-9.1 This Agreement shall be governed by and construed in accordance with the laws of [Jurisdiction]. Any disputes arising out of or relating to this Agreement shall be subject to the exclusive jurisdiction of the courts of [Jurisdiction].
-
-Severability
-10.1 If any provision of this Agreement is held to be invalid, illegal, or unenforceable, the validity, legality, or enforceability of the remaining provisions shall not be affected or impaired.</p>
-    
-    </div>
+                <MDBRow> <MDBTypography tag="h5" className="mb-3 " >Governing Law   </MDBTypography></MDBRow>
+                <MDBRow className="mb-3">
+                  <MDBCol md="1" className="mb-1">9.1 </MDBCol>
+                  <MDBCol md="9" >This Agreement shall be governed by and construed in accordance with the laws of [Jurisdiction]. Any disputes arising out of or relating to this Agreement shall be subject to the exclusive jurisdiction of the courts of [Jurisdiction].
+                  </MDBCol>   
+                </MDBRow> 
+            
+                <MDBRow> <MDBTypography tag="h5" className="mb-3 " >Severability</MDBTypography></MDBRow>
+                <MDBRow className="mb-0">
+                  <MDBCol md="1" className="mb-1">10.1 </MDBCol>
+                  <MDBCol md="9" >If any provision of this Agreement is held to be invalid, illegal, or unenforceable, the validity, legality, or enforceability of the remaining provisions shall not be affected or impaired.
+                  </MDBCol>   
+                </MDBRow>
+                <MDBRow className="term-backBtn">
+                  <Link to={`/signin`}>
+                    <MDBBtn className='terms-backBtn'>Back</MDBBtn>
+                  </Link>
+                </MDBRow>          
+              </MDBCardBody>
+          </MDBCard>
+      </div>
   )
 }
 
