ffba178ca13bec25d09de6231942741451033220
bowersjuan <bowersjuan@gmail.com>
2023-06-11 19:04:41 -0400

    Update MessagingNav with axios call for user profile img

--- src/Components/Messaging/MessagingNav.js
+++ src/Components/Messaging/MessagingNav.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import { Link } from "react-router-dom"
+import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useContextAuthProvider } from '../../Firebase/context';
 
 import "./Sidebar.css"
 
 import logo from "../../Images/LOGO_chat.png"
 import defaultProfile from "../../Images/LOGO_favicon.png"
 
+
 function MessagingNav() {
+    const [ userImg, setUserImg ] = useState("")
+    const { user } = useContextAuthProvider()
+
+    useEffect (() => {
+        axios.get(`/users/${user.uid}/images/1`, ) 
+        .then((res) => setUserImg(res.data.profile_image))
+        .catch((e) => console.error(e))
+      
+        
+
+    }, [userImg])
+
     return (
         <div className="messaging-nav">
             <img id="chat-mate-logo" src={logo} width="70px" alt="ChatMate"/>
