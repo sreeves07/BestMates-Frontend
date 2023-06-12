4c6360b52d8893755133735e95dfd9b9b6bac221
bowersjuan <bowersjuan@gmail.com>
2023-06-12 01:03:39 -0400

    Update Messaging and chat logo border radius

--- src/Components/Messaging/MessagingNav.js
+++ src/Components/Messaging/MessagingNav.js
@@ -20,15 +20,19 @@ function MessagingNav() {
         .then((res) => setUserImg(res.data[0].profile_image))
         .catch((e) => console.error(e))
       
-        
+        axios.get(`${API}/user/${user.uid}`, ) 
+        .then((res) => setUserImg(res.data[0].profile_image))
+        .catch((e) => console.error(e))
 
     }, [])
 
     return (
         <div className="messaging-nav">
             <img id="chat-mate-logo" src={logo} width="70px" alt="ChatMate"/>
+            
             <div className='exit-profile-messaging-nav'>
-                <img src={userImg} style={{borderRadius: "100%", border: "2px solid var(--mainColor)"}} width="40px" alt="user" />
+                <div style={{paddingRight: "6px", color: "white"}}>Name</div>
+                <img src={userImg} style={{borderRadius: "100%", border: "2px solid var(--mainColor)", marginRight: "5px"}} width="40px" alt="user" />
                 <Link 
                   style={{margin: "0px", paddingRight: "6px",    paddingLeft: "6px"}} 
                   className="sign-in-btn" 
