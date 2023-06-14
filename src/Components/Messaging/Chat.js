1d943185bacf1ed778e374d1d4ba74bafc5b11a4
bowersjuan <bowersjuan@gmail.com>
2023-06-14 06:56:06 -0400

    Update ChatApp

--- src/Components/Messaging/Chat.js
+++ src/Components/Messaging/Chat.js
@@ -1,31 +1,48 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-import Messages from "./Messages"
-import ChatInput from "./Input"
-import { AiFillVideoCamera } from 'react-icons/ai'
-import { FiMoreHorizontal } from 'react-icons/fi'
-import "./Chat.css"
-import { useContextChatProvider } from './ChatContext';
+import Messages from "./Messages";
+import ChatInput from "./Input";
+import { AiFillVideoCamera } from "react-icons/ai";
+import { FiMoreHorizontal } from "react-icons/fi";
+import { IoMdClose } from "react-icons/io";
+import "./Chat.css";
+import { useContextChatProvider } from "./ChatContext";
 
 function Chat(props) {
-    const { data } = useContextChatProvider()
+  const { data } = useContextChatProvider();
 
-    return (
-        <div className='chat'>
-            <div className='chat-info'>
-                <span style={{paddingLeft: '10px'}}>To: <span style={{color: "var(--darkMainColor)", fontSize: "20px", paddingLeft: "10px"}}>{data.user?.displayName}</span></span>
-                <div className='chat-icons'>
-                    <AiFillVideoCamera id='cam-icon' size='30'/>
-                    <FiMoreHorizontal id='more-icon' size='30'/>
-                    <Link to="/users" id="exit-chat-btn" className="sign-in-btn">
-                        Exit
-                    </Link>
-                </div>
-            </div>
-            <Messages />
-            <ChatInput />
+  return (
+    <div className="chat">
+      <div className="chat-info">
+        <div className="chat-top-bar">
+          <span style={{ fontSize: "15px" }}>To:</span>
+          {data.user.displayName && (
+            <>
+              <span
+                style={{
+                  color: "var(--darkMainColor)",
+                  fontSize: "15px",
+                  paddingLeft: "10px",
+                }}>
+                {data.user.displayName}
+              </span>
+              <img width="40px" src={data.user.photoURL} />
+            </>
+          )}
         </div>
-    );
+        <div className="chat-icons">
+          <AiFillVideoCamera id="cam-icon" size="30" />
+          <br></br>
+          <FiMoreHorizontal id="more-icon" size="30" />
+          <Link to="/users" id="exit-chat-btn">
+            <IoMdClose size="30" />
+          </Link>
+        </div>
+      </div>
+      <Messages />
+      <ChatInput />
+    </div>
+  );
 }
 
 export default Chat;
